e70f5c091dde2f74082f64a9db84cd160c1bf8a0
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-07-06 15:24:20 +0300

    changeg from dropdown menu to text, fixed map loc

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends React.Component {
 
       pValue: "",
       showP: false,
+
+      cityName: "",
     };
   }
 
@@ -23,41 +25,42 @@ class App extends React.Component {
     event.preventDefault();
 
     console.log('hello');
-    console.log(event);
-    console.log(event.target);
-    console.log(event.target.value);
+    console.log('event = ',event);
+    console.log('event.target = ',event.target);
+    console.log('event.target.city = ',event.target.city);
+    console.log('event.target.city.value = ',event.target.city.value);
+    // console.log(event.target.value);
 
     this.setState({
       pValue: event.target.value,
       showP: true,
     });
+    await this.setState ({
+      cityName: event.target.city.value
+    })
+    let locationIqUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${event.target.city.value}&format=json`;
 
-    let locationIqUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${event.target.value}&format=json`;
+    
     let resData = await axios.get(locationIqUrl);
-
-    this.setState({
-      cityObj: resData.data[1],
+    console.log(resData);
+    await this.setState({
+      cityObj: resData.data[0],
       mapVisible: true,
+      
     });
   };
   render() {
     return (
       <div className="App">
         <Header />
-        <Form>
-          <Form.Group controlId="exampleForm.SelectCustom">
-            <Form.Label>Select City:</Form.Label>
-            <Form.Control as="select" custom onChange={this.selectCity}>
-              <option value="irbid">Irbid</option>
-              <option value="amman">Amman</option>
-              <option value="aqaba">Aqaba</option>
-            </Form.Control>
-          </Form.Group>
+        <Form  onSubmit={this.selectCity}>
+          <input type="text" name="city" />
+          <button type="submit" >Explore city</button>
         </Form>
         <div>
           City Name: {this.state.cityObj.display_name},{this.state.cityObj.lat},{this.state.cityObj.lon}
         </div>
-        {this.state.mapVisible && <img alt="" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityObj.lat},${this.state.cityObj.lon}&zoom=20&size=400x400&format=jpg&maptype=roadmap`} />}
+        {this.state.mapVisible && <img alt="" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityObj.lat},${this.state.cityObj.lon}&&zoom=13&size=600x600&format=jpg&maptype=roadmap`} />}
         {this.state.showP && <p>{this.state.pValue}</p>}
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,16 +8,7 @@ class Main extends React.Component {
   render() {
     return (
       <div className="Main">
-        <Form>
-          <Form.Group controlId="exampleForm.SelectCustom">
-            <Form.Label>Select City:</Form.Label>
-            <Form.Control as="select" custom onChange={this.props.selectCity}>
-              <option value="Irbid">Irbid</option>
-              <option value="Amman">Amman</option>
-              <option value="Aqaba">Aqaba</option>
-            </Form.Control>
-          </Form.Group>
-        </Form>
+        
       </div>
     );
   }
